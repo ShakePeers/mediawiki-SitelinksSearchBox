eda67932d1488d457fe47d19d5fcd71f9d87e4d6
Pierre Rudloff <contact@rudloff.pro>
2015-01-25 18:30:41 +0100

    Initial commit

---
+++ SitelinksSearchBox.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * SitelinksSearchBox
+ * Provide Sitelinks Search Box to Google
+ *
+ * PHP version 5.4
+ *
+ * @category Extension
+ * @package  SitelinksSearchBox
+ * @author   Pierre Rudloff <contact@rudloff.pro>
+ * @license  GPL http://www.gnu.org/licenses/gpl.html
+ * @link     https://github.com/ShakePeers/mediawiki-SitelinksSearchBox
+ * */
+$wgExtensionCredits['validextensionclass'][] = array(
+   'name' => 'SitelinksSearchBox',
+   'author' =>'ShakePeers', 
+   'url' => 'http://shakepeers.org/'
+);
+
+/**
+ * Add JSON-LD to head
+ *
+ * @param OutputPage $out HTML page
+ * 
+ * @return void
+ * */
+function sitelinksSearchBox(&$out)
+{
+    global $wgScriptPath, $wgServer;
+    $out->addHeadItem(
+        'SitelinksSearchBox',
+        '<script type="application/ld+json">
+        {
+           "@context": "http://schema.org",
+           "@type": "WebSite",
+           "url": "'.$wgServer.$wgScriptPath.'",
+           "potentialAction": {
+             "@type": "SearchAction",
+             "target": "'.$wgServer.$wgScriptPath.'/index.php?search={search}",
+             "query-input": "required name=search"
+           }
+        }
+        </script>'
+    );
+}
+
+$wgHooks['BeforePageDisplay'][] = 'sitelinksSearchBox';
+?>
